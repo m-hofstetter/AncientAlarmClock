c591a8700781f13b3d547d1d58c83cd579e07840
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-13 21:49:12 +0200

    Add bounce time to buttons

--- components/buttons.py
+++ components/buttons.py
@@ -1,15 +1,15 @@
 from gpiozero import Button
 
 BUTTONS = [
-    Button(9, pull_up=False),
-    Button(11, pull_up=False),
-    Button(10, pull_up=False),
-    Button(5, pull_up=False)
+    Button(9, pull_up=False, bounce_time=0.05),
+    Button(11, pull_up=False, bounce_time=0.05),
+    Button(10, pull_up=False, bounce_time=0.05),
+    Button(5, pull_up=False, bounce_time=0.05)
 ]
 
 TOGGLE_SWITCHES = [
-    Button(26, pull_up=False),
-    Button(19, pull_up=False),
-    Button(13, pull_up=False),
-    Button(20, pull_up=False)
+    Button(26, pull_up=False, bounce_time=0.05),
+    Button(19, pull_up=False, bounce_time=0.05),
+    Button(13, pull_up=False, bounce_time=0.05),
+    Button(20, pull_up=False, bounce_time=0.05)
 ]
