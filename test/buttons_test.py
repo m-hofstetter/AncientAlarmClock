a104f699e2e2480b3de4c813b4750ea8c3b0aed4
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-13 23:07:14 +0200

    Add buttons test

---
+++ test/buttons_test.py
@@ -0,0 +1,20 @@
+from asyncio import wait
+from signal import pause
+
+from gpiozero import Button
+
+from components.buttons import BUTTONS, TOGGLE_SWITCHES
+from components.displays import DISPLAYS, Display
+
+
+def define_action_for_press(button: Button, display: Display):
+    button.when_activated = lambda: display.show_text('ON')
+    button.when_deactivated = lambda: display.show_text('OFF')
+
+for i in range(4):
+    define_action_for_press(TOGGLE_SWITCHES[i], DISPLAYS[i])
+
+for i in range(4):
+    define_action_for_press(BUTTONS[i], DISPLAYS[i + 4])
+
+pause()
