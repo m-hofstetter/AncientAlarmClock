7b0aaefa723fd95facbcf5abdda0c753d6997f82
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-13 12:53:19 +0200

    Improve TTS

--- components/speaker.py
+++ components/speaker.py
@@ -1,19 +1,25 @@
+import uuid
 import wave
 import subprocess
+import os
 from piper import PiperVoice
 
-# 1) Load the voice once
-voice = PiperVoice.load("../voices/de_DE-thorsten-high.onnx")
+os.sched_setaffinity(0, {0, 1})
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
-print('Geladen')
+voice = PiperVoice.load("../voices/de_DE-thorsten-medium.onnx")
 
-def say(text: str, filename="out.wav"):
-    """Generate speech with Piper and save it to a WAV file, then play it."""
+def generate_speech(text: str):
+    filename = f"../generated_speech/{uuid.uuid4()}.wav"
+
     with wave.open(filename, "wb") as wf:
         voice.synthesize_wav(text, wf)
 
-    # 2) Play the WAV file with ALSA
+    return filename
+
+def say(filename):
     subprocess.run(["aplay", filename])
 
-# Example:
-say("Guten Morgen! Ich bin Piper und spreche jetzt direkt über die Datei.")
+def generate_and_say(text):
+    say(generate_speech(text))
