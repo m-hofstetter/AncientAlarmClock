9cc15ae0dcc051096e1756eb1490e0c996a729d4
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-13 20:20:36 +0200

    Update potentiometer test

--- test/potentiometer_test.py
+++ test/potentiometer_test.py
@@ -7,6 +7,6 @@ from components.potentiometers import Potentiometers
 pots = Potentiometers()
 
 while True:
-    DISPLAYS[8].write(f"{pots.get_percent(0):.1f}")
-    DISPLAYS[9].write(f"{pots.get_percent(1):.1f}")
-    DISPLAYS[10].write(f"{pots.get_percent(2):.1f}")
+    DISPLAYS[8].show_text(f"{pots.get_percent(0):.1f}")
+    DISPLAYS[9].show_text(f"{pots.get_percent(1):.1f}")
+    DISPLAYS[10].show_text(f"{pots.get_percent(2):.1f}")
