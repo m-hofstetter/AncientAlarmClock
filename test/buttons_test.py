24a11840eb2894aeb06c259ae5f2109cd8ee4ee5
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-14 11:46:16 +0200

    Improve buttons test

--- test/buttons_test.py
+++ test/buttons_test.py
@@ -12,9 +12,11 @@ def define_action_for_press(button: Button, display: Display):
     button.when_deactivated = lambda: display.show_text('OFF')
 
 for i in range(4):
+    DISPLAYS[i].show_text('ON' if TOGGLE_SWITCHES[i].value else 'OFF')
     define_action_for_press(TOGGLE_SWITCHES[i], DISPLAYS[i])
 
 for i in range(4):
+    DISPLAYS[i + 4].show_text('ON' if BUTTONS[i].value else 'OFF')
     define_action_for_press(BUTTONS[i], DISPLAYS[i + 4])
 
 pause()
