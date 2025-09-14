b5881ad324095d7ded9b4a55776e20abafc0c3ee
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-14 14:23:37 +0200

    Add online TTS and change playback method

--- components/speaker.py
+++ components/speaker.py
@@ -1,8 +1,8 @@
 import uuid
 import wave
-import subprocess
 import os
 from piper import PiperVoice
+from gtts import gTTS
 
 os.sched_setaffinity(0, {0, 1})
 os.environ["OMP_NUM_THREADS"] = "1"
@@ -10,16 +10,23 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
 voice = PiperVoice.load("../voices/de_DE-thorsten-medium.onnx")
 
-def generate_speech(text: str):
-    filename = f"../generated_speech/{uuid.uuid4()}.wav"
+LOCAL = True # Whether to use a local TTS (Piper) or online (gTTS)
 
-    with wave.open(filename, "wb") as wf:
-        voice.synthesize_wav(text, wf)
+def generate_speech(text: str):
+
+    if LOCAL:
+        filename = f"../generated_speech/{uuid.uuid4()}.wav"
+        with wave.open(filename, "wb") as wf:
+            voice.synthesize_wav(text, wf)
+    else:
+        filename = f"../generated_speech/{uuid.uuid4()}.mp3"
+        tts = gTTS(text=text, lang='de')
+        tts.save(filename)
 
     return filename
 
 def say(filename):
-    subprocess.run(["aplay", filename])
+    os.system(f"xdg-open {filename}")
 
 def generate_and_say(text):
     say(generate_speech(text))
