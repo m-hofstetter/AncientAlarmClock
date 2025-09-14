ddee39f8824b832039c8790e0aceff6122b62364
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-14 16:13:57 +0200

    Start Lights out via HTTP

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from components.buttons import TOGGLE_SWITCHES
+from components.displays import DISPLAYS
+from riddles.lightsout import LightsOut
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class LightsOutRequest(BaseModel):
+    correctGlyph: str
+    incorrectGlyph: list[str]
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/lights_out")
+async def say_hello(body: LightsOutRequest):
+
+    l = LightsOut(
+        body.correctGlyph,
+        body.incorrectGlyph,
+        TOGGLE_SWITCHES,
+        DISPLAYS[:4]
+    )
+    l.start()
+
+    print(body)
+    return {"message": f"Hello"}
