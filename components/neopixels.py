fd720e31af9ecfd35a3f4abc9bed784258dfead2
Maximilian Hofstetter <maximilian.hofstetter@gmx.net>
2025-09-14 10:07:59 +0200

    Parameterize neopixels

--- components/neopixels.py
+++ components/neopixels.py
@@ -1,10 +1,12 @@
 import threading
 from time import sleep
+from typing import Callable
 
 import board, neopixel, time, math, colorsys
 
 PIXEL_PIN = board.D16
 NUM_PIXELS = 56
+
 
 class Neopixels:
     def __init__(self):
@@ -13,16 +15,16 @@ class Neopixels:
         self.__thread = None
 
     def __scaled_sin(self, x, average, amplitude, period):
-        return average + amplitude/2 * math.sin(x * 1/period)
+        return average + amplitude / 2 * math.sin(x * 1 / period)
 
     def __scaled_sin_of_time(self, x, average, amplitude, period):
         t = time.time_ns() / 100_000_000
-        return  self.__scaled_sin(t + x, average, amplitude, period)
+        return self.__scaled_sin(t + x, average, amplitude, period)
 
-    def __green_oscillate_per_pixel(self, pixel_number):
+    def __oscillate_per_pixel(self, pixel_number, color):
 
-        global_hue_factor = self.__scaled_sin_of_time(0, 1, 1, 5)
-        hue = self.__scaled_sin_of_time(pixel_number, 0.36, 0.05, 11) * global_hue_factor
+        global_hue_factor = self.__scaled_sin_of_time(0, 1, 0.2, 5)
+        hue = self.__scaled_sin_of_time(pixel_number, color, 0.05, 11) * global_hue_factor
 
         saturation = self.__scaled_sin_of_time(pixel_number, 0.95, 0.1, 3)
 
@@ -34,19 +36,22 @@ class Neopixels:
         r, g, b = colorsys.hsv_to_rgb(hue % 1.0, max(0, min(1, saturation)), max(0, min(1, brightness)))
         return int(r * 255), int(g * 255), int(b * 255)
 
-    def green_oscillate_update_all_pixels(self):
+    def __update_all_pixels(self, per_pixel: Callable[[int], tuple[int, int, int]]):
         for i in range(NUM_PIXELS):
-            self.pixels[i] = self.__green_oscillate_per_pixel(i)
+            self.pixels[i] = per_pixel(i)
         self.pixels.show()
 
-    def __ambient_light_thread(self):
+    def __continuous_light_thread(self, per_pixel: Callable[[int], tuple[int, int, int]]):
         while self.__running:
-            self.green_oscillate_update_all_pixels()
+            self.__update_all_pixels(per_pixel)
             sleep(0.01)
 
-    def start(self):
+    def start_continuous(self, color=0.5):
         self.__running = True
-        self.__thread = threading.Thread(target=self.__ambient_light_thread)
+        self.__thread = threading.Thread(
+            target=lambda: self.__continuous_light_thread(
+                lambda x: self.__oscillate_per_pixel(x, color)
+            ))
         self.__thread.start()
 
     def stop(self):
@@ -55,3 +60,10 @@ class Neopixels:
             self.__thread.join()
         self.pixels.fill((0, 0, 0))
         self.pixels.show()
+
+
+if __name__ == "__main__":
+    n = Neopixels()
+    n.start_continuous()
+    sleep(5)
+    n.stop()
